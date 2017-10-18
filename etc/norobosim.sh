4a1c6d64454a6cd80d4626ed66066fddf2ebb68c
David Norton <dgnorton@gmail.com>
2017-10-18 09:16:39 -0400

    fix norobosim script

--- etc/norobosim.sh
+++ etc/norobosim.sh
@@ -2,14 +2,16 @@
 
 set -e
 
-OPTIND=1
 socatPID=0
 tmpdir="/tmp/norobo"
 session="norobo"
+twilioSID=
+twilioTok=
 
-while getopts "k" opt; do
-	case "$opt" in
-		k)
+# Parse command line arguments.
+while :; do
+	case "$1" in
+		-k|--kill)
 			# Kill socat if it's running and exit this script.
 			socatPID="$(pidof socat)"
 			if [ "$socatPID" -gt 1 ]; then
@@ -17,10 +19,34 @@ while getopts "k" opt; do
 			fi
 			exit 0
 			;;
+		--twilio)
+			# Parse Twilio SID and token.
+			if [ "$2" ]; then
+				twilioSID=$(echo $2 | cut -d: -f1)
+				twilioTok=$(echo $2 | cut -d: -f2)
+
+				if [ -z "$twilioSID" -o -z "$twilioTok" ]; then
+					echo "--twilio requires SID:Token"
+					exit 1
+				fi
+			else
+				echo "--twilio requires SID:Token"
+				exit 1
+			fi
+			shift
+			;;
+		*)
+			break
 	esac
+
+	shift
 done
 
-shift $((OPTIND-1))
+# Make sure socat is installed.
+if [ -z "$(which socat)" ]; then
+	echo "error: socat must be installed for this script to run"
+	exit 1
+fi
 
 # Delete old tmp work dir if it exists.
 if [ -d "$tmpdir" ]; then
@@ -40,26 +66,30 @@ cp $GOPATH/src/github.com/dgnorton/norobo/filter/exec_example.sh $tmpdir
 # Change to the tmp working dir.
 cd $tmpdir
 
-# Create pair of linked TTYs / serial ports.
-socat -d -d pty,raw,echo=0 pty,raw,echo=0 > $tmpdir/socat.out 2>&1 &
-sleep 1
-
-# Parse TTY names from socat's output.
-modemTTY="$(cat $tmpdir/socat.out | head -1 | awk '{print $NF }')"
-norobodTTY="$(cat $tmpdir/socat.out | head -2 | tail -1 | awk '{print $NF }')"
-
 # Setup tmux session with three panes: 1 for modem sim, 1 for norobod, and 1 for user interaction.
 tmux new-session -d -s $session
 tmux split-window -h -t $session -p 50
 tmux select-pane -t $session:0.0
 tmux split-window -v -t $session -p 25
 
+# Create pair of linked TTYs / serial ports.
+tmux send-keys -t $session:0.0 "socat -d -d pty,raw,echo=0 pty,raw,echo=0 > $tmpdir/socat.out 2>&1 &" C-m
+sleep 1
+
+# Parse TTY names from socat's output.
+modemTTY="$(cat $tmpdir/socat.out | head -1 | awk '{print $NF }')"
+norobodTTY="$(cat $tmpdir/socat.out | head -2 | tail -1 | awk '{print $NF }')"
+
 # Start the modem simulator and wait for it to initialize.
 tmux send-keys -t $session:0.0 "$tmpdir/modem -c \"$modemTTY,19200,n,8,1\"" C-m
 sleep 1
 
 # Start norobod so that it uses the modem simulator.
-tmux send-keys -t $session:0.2 "$tmpdir/norobod -c \"$norobodTTY,19200,n,8,1\" -block $tmpdir/block.csv -call-log $tmpdir/call.log -exec $tmpdir/exec_example.sh" C-m
+if [ ! -z "$twilioSID" ]; then
+	twilioOpts="-twlo-sid $twilioSID -twlo-token $twilioTok"
+fi
+
+tmux send-keys -t $session:0.2 "$tmpdir/norobod -c \"$norobodTTY,19200,n,8,1\" -block $tmpdir/block.csv -call-log $tmpdir/call.log -exec $tmpdir/exec_example.sh $twilioOpts" C-m
 
 # Set the active tmux pane to the one for user interaction.
 tmux select-pane -t $session:0.1
